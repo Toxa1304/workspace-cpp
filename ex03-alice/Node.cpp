b1575043754f70a3a3fbf15eacc55d711bf41d4d
Antons Jaroslavcevs <jar.toxatoxa@gmail.com>
2020-09-28 06:15:50 +0000

    Almosst done

--- ex03-alice/Node.cpp
+++ ex03-alice/Node.cpp
@@ -1,1 +1,46 @@
+#include "Node.h"
 
+using namespace std;
+using namespace ds_course;
+
+Node::Node(int vv){
+  this->val = vv;
+  vector<int> children;
+  bool isInList = false;
+}
+
+void Node::changeValue(int value){
+  this->val = value;
+}
+void Node::addChild(int value){
+  children.push_back(value);
+ // cout << children[0] << endl;
+  //cout << "value is "<< children[1].val << endl;
+   
+}
+void Node::reflect(){
+  std::reverse(children.begin(), children.end());
+}
+void Node::printPreorder()
+{
+    cout << val << " ";
+    for (auto &element : children)
+        cout << element << ' ';
+    cout << endl;
+}
+
+/*
+struct Node {
+  private:
+  int val;
+  vector<Node *> chilren;
+  public:
+  Node(int vv);
+  void addChild(Node nn);
+  void reflect();
+  int getVal();
+  vector<int> getChildren();
+
+
+};
+*/
--- ex03-alice/Node.h
+++ ex03-alice/Node.h
@@ -1,1 +1,20 @@
-
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <fstream>
+#include <algorithm> 
+using namespace std;
+namespace ds_course {
+struct Node {
+  public:
+  int val;
+  vector<int> children;
+  bool isInList;
+  void addChild(int value);
+  Node(int value);
+  void changeValue(int value);
+  void reflect();
+  void printPreorder();
+};
+}
